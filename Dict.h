95512fb03710d8a89f692bc91ecd1e03b5a16cf3
kkorolo <kkorolo@etsinf.upv.es>
2025-11-27 17:31:02 +0100

    Añadida interfaz genérica Dict

---
+++ Dict.h
@@ -0,0 +1,23 @@
+#ifndef DICT_H
+#define DICT_H
+#include <string>
+template <typename V>
+class Dict
+{
+public:
+    virtual void insert(std::string key, V value);/*Inserta el par key->value en el diccionario. 
+    Lanza una std::runtime_error si key ya existe en el diccionario.*/
+    virtual V search(std::string key);/*Busca el valor correspondiente a key. Devuelve el value asociado si key está en diccionario. 
+            Si no se encuentra, lanza una std::runtime_error.*/
+    virtual V remove(std::string key);/*Elimina el par key->value si se encuentra en el diccionario. 
+    Si no se encuentra, lanza una std::runtime_error*/
+    virtual int entries();//Devuelve el número de elementos que tiene el Diccionario.
+
+
+
+
+
+};
+#endif
+
+
